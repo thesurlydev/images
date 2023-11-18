8b8e7e0caef2e3a04c5533e3fac8aa0e12a6cb4d
Shane Witbeck <shane@digitalsanctum.com>
2023-11-18 10:41:40 -0800

    feat: allow spring boot dependency management without requiring main class

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-//	id("org.springframework.boot") version "3.1.5"
-//	id("io.spring.dependency-management") version "1.1.3"
+	id("org.springframework.boot") version "3.1.5" apply false
+	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 }
@@ -18,10 +18,24 @@ repositories {
 	mavenCentral()
 }
 
+// This is part of using spring's dependency management in isolation.
+//      See https://docs.spring.io/spring-boot/docs/2.4.2/gradle-plugin/reference/htmlsingle/#managing-dependencies-dependency-management-plugin-using-in-isolation
+the<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension>().apply {
+	imports {
+		mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+	}
+}
+
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")/* {
+		exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+	}*/
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-json")
+	implementation("org.postgresql:r2dbc-postgresql")
+	implementation("org.slf4j:slf4j-api")
+	implementation("ch.qos.logback:logback-classic")
 	implementation("io.nats:jnats:2.17.1")
-	implementation("org.slf4j:slf4j-api:2.0.9")
-	implementation("ch.qos.logback:logback-classic:1.4.11")
 }
 
 tasks.withType<KotlinCompile> {
@@ -34,5 +48,3 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
-
